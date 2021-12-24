1c0baed0583e693a244f04dfb38a1d032281d72c
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2021-12-24 08:40:51 +0000

    feat: C# | add solution for problem #6

--- CSharp/ProblemSolver.cs
+++ CSharp/ProblemSolver.cs
@@ -339,5 +339,40 @@ namespace ProjectEuler
 
             return currentMinimum;
         }
+
+        /// <summary>
+        /// -------------------------------------------------------------------
+        /// Problem 6
+        /// Sum square difference
+        /// -------------------------------------------------------------------
+        /// The sum of the squares of the first ten natural numbers is:
+        ///    1*1 + 2*2 + ... + 10*10 = 385
+        ///    
+        /// The square of the sum of the first ten natural numbers is:
+        ///    (1+2+...+10)^2 = 55^2 = 2640
+        ///    
+        /// Hence the difference between the sum of the squares of the first
+        /// ten natural numbers and the square of the sum is:
+        ///        3025 - 385 = 2640
+        ///    
+        /// Find the difference between the sum of the squares of the first
+        /// one hundred natural numbers and the square of the sum.
+        /// -------------------------------------------------------------------
+        /// </summary>
+        /// <returns>Solution for Project Euler #6</returns>
+        public long SolveProblem0006()
+        {
+            long targetNumber = 100;
+            long squaresSum = 0;
+            long simpleSum = 0;
+
+            for (long i = 1; i <= targetNumber; i++)
+            {
+                squaresSum += i * i;
+                simpleSum += i;
+            }
+
+            return (simpleSum * simpleSum) - squaresSum;
+        }
     }
 }
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -16,6 +16,7 @@ namespace ProjectEuler
             new ProblemInfo() { Number = 3, Title = "Largest prime factor", HasSlowResolution = true, ExpectedSolution = 6857 },
             new ProblemInfo() { Number = 4, Title = "Largest palindrome product", HasSlowResolution = false, ExpectedSolution = 906609 },
             new ProblemInfo() { Number = 5, Title = "Smallest multiple", HasSlowResolution = false, ExpectedSolution = 232792560 },
+            new ProblemInfo() { Number = 6, Title = "Sum square difference", HasSlowResolution = false, ExpectedSolution = 25164150 },
         };
 
         static void Main(string[] args)
